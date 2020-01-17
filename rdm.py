395c5e8e0af6af15090b9e56b3756e4eed1e5cc3
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-01-17 18:47:00 +0530

    Add basic code to plot data

--- rdm.py
+++ rdm.py
@@ -5,10 +5,13 @@ import numpy as np
 from matplotlib import pyplot as pyplot
 
 class Rdm:
-  def __init__(self, file_name):
+  def __init__(self, file_name, arch):
     super().__init__()
     self.file_name = file_name
+    self.arch = arch
     self.data = squareform(loadmat())
+    self.mean = find_mean()
+    self.std = find_std()
 
   def loadmat(self):
     return sio.loadmat(self.file_name)
@@ -19,4 +22,11 @@ class Rdm:
   def find_std(self):
     return np.std(self.data)
 
+  def plot(self):
+    # the data points
+    plt.scatter(x, self.data)
+    # the mean
+    plt.plot(self.mean, self.mean)
+    # save plot
+    plt.savefig(self.arch)
 
