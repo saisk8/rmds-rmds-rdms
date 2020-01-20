1cbb02d2eff93a48843770262df043e77c109a8e
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-01-20 15:51:11 +0530

    main script to go through directories

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import os
 from rdm import Rdm
 
-
 for subdir, dirs, files in os.walk('rdms'):
-  if len(dirs) == 0 and len(files) != 0:
-    # print(files)
-    # Get the architecture, image set
-    # Construct the object and call analyse method
+    if len(dirs) == 0 and len(files) != 0:
+        analyser = Rdm(subdir)
+        analyser.analyse()
