8ec0455e1d9628fa0b3599647d682ede72f266df
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-01-27 18:53:06 +0530

    Take path from sys args

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import os
+import sys
 from rdm import Rdm
 
-for subdir, dirs, files in os.walk('rdms'):
+for subdir, dirs, files in os.walk(sys.argv[1]):
     if len(dirs) == 0 and len(files) != 0:
         analyser = Rdm(subdir)
         analyser.analyse()
